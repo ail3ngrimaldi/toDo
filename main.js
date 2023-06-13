7024f8e615a1c12654d99988a7635ff627b3abfd
ail3ngrimaldi <ailengrimaldi1@gmail.com>
2023-06-12 23:47:52 -0300

    tasks shows okey can be setted and getted from/to local storage

--- main.js
+++ main.js
@@ -14,50 +14,89 @@ const toggleModal = (modal, className) => {
 // según si hay o no tareas en la lista
 const showHideInitMessage = (tasks) => {
     if (tasks.length === 0) {
-        initMessage.classList.remove('hidden');
+        initMessage.style.display = 'flex';
+        tasksContainer.style.display = 'none';
     } else {
-        initMessage.classList.add('hidden');
+        initMessage.style.display = 'none';
+        tasksContainer.style.display = 'flex';
     }
 }
 
-
-const showTasks = () => {
-    document.addEventListener('load', () => {
-        tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        showHideInitMessage(tasks);
-        if (tasks.length > 0) {
-            tasksContainer.classList.add('active');
-            tasksContainer.classList.remove('hidden');
-        } else {
-            tasksContainer.classList.remove('active');
-            tasksContainer.classList.add('hidden');
-        }
-    })
+window.onload = function() {
+    tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.forEach(task => {
+        createTaskElement(task);
+    });
+    // Muestra u oculta el mensaje inicial después de cargar las tareas
+    showHideInitMessage(tasks);
 }
 
+const createTaskElement = (task) => {
+    let selectIcon = '';
+
+    let job = './assets/job.png';
+    let entertainment = './assets/ent2.png';
+    let domestic = './assets/home1.png';
+    let personal = './assets/personal2.png';
+
+    switch (task.category) {
+        case 'Laboral':
+            selectIcon = job;
+            break;
+        case 'Personal':
+            selectIcon = personal;
+            break;
+        case 'Domestica':
+            selectIcon = domestic;
+            break;
+        case 'Entretenimiento':
+            selectIcon = entertainment;
+            break;
+    }
+
+    let addedTask = `<div class="list-item priority-container ${task.priority}">
+                        <img alt=${task.category} class="selectedIcon"src=${selectIcon}></img>
+                        <div class=task-content>        
+                            <input class="input-content" type="text" value=${task.name} readonly />
+                        </div>
+                        <div class="task-btns-container">
+                            <div class="content-container">
+                                <input class="checked-input" type="checkbox" checked=${task.done} onclick="checkItem()"/>
+                            </div>
+                            <button class="edit" title="edit" onclick="editItem()"></button>
+                            <button class="delete" title="delete" onclick="deleteItem()"></button>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><g id="Layer_20" data-name="Layer 20"><path d="M49.93,17.33H41.87V12a1.5,1.5,0,0,0-1.5-1.5H23.63a1.5,1.5,0,0,0-1.5,1.5v5.33H14.07a1.5,1.5,0,0,0,0,3H16V48a5.49,5.49,0,0,0,5.49,5.48h21.1A5.49,5.49,0,0,0,48,48V20.33h1.89A1.5,1.5,0,0,0,49.93,17.33ZM25.13,13.5H38.87v3.83H25.13ZM45,48a2.49,2.49,0,0,1-2.49,2.48H21.45A2.49,2.49,0,0,1,19,48V20.33H45Z"/><path d="M28,25.87a1.5,1.5,0,0,0-1.5,1.5V43.46a1.5,1.5,0,1,0,3,0V27.37A1.5,1.5,0,0,0,28,25.87Z"/><path d="M36,25.87a1.5,1.5,0,0,0-1.5,1.5V43.46a1.5,1.5,0,1,0,3,0V27.37A1.5,1.5,0,0,0,36,25.87Z"/></g></svg>
+                            </button>
+                        </div>
+                    </div>`;
+
+    tasksContainer.insertAdjacentHTML('beforeend', addedTask);
+}
+
+// Event listener que abre el formulario
 openFormButton.addEventListener('click', () => {
     toggleModal(modal, 'active');
     tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-    initMessage.classList.add('hidden');
+    tasksContainer.style.display = 'none';  // Ocultar el contenedor de tareas
 });
 
-addTask()
 
-
-//Close form
-    const closeForm = () => {
-        modal.classList.toggle('active')
-        let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-        showHideInitMessage(tasks);
-        initMessage.classList.toggle('hidden')
+// Cerrar el formulario
+const closeForm = () => {
+    modal.classList.toggle('active');
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    if(tasks.length > 0) {
+        tasksContainer.style.display = 'flex';  // Muestra el contenedor de tareas si hay tareas
     }
+    showHideInitMessage(tasks);
+}
 
 let closeFormButton = document.querySelector('#close-form').addEventListener('click', closeForm)
 let cancelButton = document.querySelector('#cancel-add-task').addEventListener('click', closeForm)
 
 //Add task
-function addTask () {
-let form = document.querySelector('#form')
+function addTask() {
+    let form = document.querySelector('#form')
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
@@ -68,74 +107,33 @@ let form = document.querySelector('#form')
             category: e.target.elements.clasification.value,
             priority: taskPriority,
             done: false
-            }
-        
-        //Añadimos la tarea a localStorage:
+        }
+
         let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
         tasks.push(task)
         localStorage.setItem('tasks', JSON.stringify(tasks));
 
-        //Añadimos la tarea al container de tareas:
-        const tasksContainer = document.querySelector('#tasks-display');
-        
-        tasks.forEach(task => {
+        // Llama a la función que crea el elemento de la tarea y la añade al DOM
+        createTaskElement(task);
 
-            let selectIcon = '';
-        
-            let job = './assets/job.png';
-            let entertainment = './assets/ent2.png';
-            let domestic = './assets/home1.png';
-            let personal = './assets/personal2.png';
-        
-            let select = document.querySelector('#clasification'); //se puede poner el .value aca, cuando este todo refactorizar.
-            let taskCategory = select.value;
-        
-            switch (taskCategory) {
-                case 'Laboral':
-                selectIcon = job;
-                break
-                case 'Personal':
-                selectIcon = personal;
-                break
-                case 'Domestica':
-                selectIcon = domestic;
-                break
-                case 'Entretenimiento':
-                selectIcon = entertainment;
-                break
-            }
-        
-            let addedTask = `<div class="list-item priority-container ${task.priority}">
-                                <img alt=${task.category} class="selectedIcon"src=${selectIcon}></img>
-                                <div class=task-content>        
-                                    <input class="input-content" type="text" value=${task.name} readonly />
-                                </div>
-                                        <div class="task-btns-container">
-                                        <div class="content-container">
-                                            <input class="checked-input" type="checkbox" checked=${task.done} onclick="checkItem()/>
-                                            </div>
-                                            <button class="edit" title="edit" onclick="editItem()">
-                                            <button class="delete" title="delete" onclick="deleteItem()">
-                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><g id="Layer_20" data-name="Layer 20"><path d="M49.93,17.33H41.87V12a1.5,1.5,0,0,0-1.5-1.5H23.63a1.5,1.5,0,0,0-1.5,1.5v5.33H14.07a1.5,1.5,0,0,0,0,3H16V48a5.49,5.49,0,0,0,5.49,5.48h21.1A5.49,5.49,0,0,0,48,48V20.33h1.89A1.5,1.5,0,0,0,49.93,17.33ZM25.13,13.5H38.87v3.83H25.13ZM45,48a2.49,2.49,0,0,1-2.49,2.48H21.45A2.49,2.49,0,0,1,19,48V20.33H45Z"/><path d="M28,25.87a1.5,1.5,0,0,0-1.5,1.5V43.46a1.5,1.5,0,1,0,3,0V27.37A1.5,1.5,0,0,0,28,25.87Z"/><path d="M36,25.87a1.5,1.5,0,0,0-1.5,1.5V43.46a1.5,1.5,0,1,0,3,0V27.37A1.5,1.5,0,0,0,36,25.87Z"/></g></svg>
-                                            </button>
-                                        </div>
-                            </div>`;
-            
-        tasksContainer.insertAdjacentHTML('beforeend', addedTask)
+        // Muestra u oculta el mensaje inicial según corresponda
+        showHideInitMessage(tasks);
+
         e.target.reset();
     })
-})
 }
+document.querySelector('#add-task').addEventListener('click', addTask);
+
 
 const deleteItem = () => { 
-    var currentTasks = document.querySelectorAll(".delete");
-        for(var i=0; i < currentTasks.length; i++){
-        currentTasks[i].onclick = function(){
-            this.parentNode.remove();
-            dayOff.style.display = 'flex';
-            tasksContainer.style.display = 'none';
-            //openForm.style.display = 'flex'
-        }
-    }
+    alert('you deleted me');
+    showHideInitMessage(tasks);
 }
 
+const checkItem = () => { 
+   alert('you checked me');
+}
+
+const editItem = () => { 
+    alert('you edited me');
+}
